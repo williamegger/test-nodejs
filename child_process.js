c8e91ee7cb043d5785b5c0784fc86f55c4eca711
williamegger <625892330@qq.com>
2016-10-24 14:02:24 +0800

    test2

--- child_process.js
+++ child_process.js
@@ -1,26 +1,26 @@
 var child_process = require('child_process');
 var iconvlite = require('iconv-lite');
 
-// var workProcess;
-// if (process.platform == 'win32') {
-//     workProcess = child_process.spawn('cmd.exe', ['/s', '/c', 'dir']);
-// } else {
-//     workProcess = child_process.spawn('sh', ['-c', 'dir']);
-// }
-// workProcess.stdout.on('data', function (data) {
-//     console.log('stdout.on("data") ===========================');
-//     console.log(iconvlite.decode(data, 'cp936'));
-// });
-// workProcess.stderr.on('data', function (data) {
-//     console.log('stderr.on("data") ===========================');
-//     console.log(iconvlite.decode(data, 'cp936'));
-// });
-// workProcess.on('error', function (err) {
-//     console.log('error:', err);
-// });
-// workProcess.on('close', function (code) {
-//     console.log('close:', code);
-// });
+var workProcess;
+if (process.platform == 'win32') {
+    workProcess = child_process.spawn('cmd.exe', ['/s', '/c', 'dir']);
+} else {
+    workProcess = child_process.spawn('sh', ['-c', 'dir']);
+}
+workProcess.stdout.on('data', function (data) {
+    console.log('stdout.on("data") ===========================');
+    console.log(iconvlite.decode(data, 'cp936'));
+});
+workProcess.stderr.on('data', function (data) {
+    console.log('stderr.on("data") ===========================');
+    console.log(iconvlite.decode(data, 'cp936'));
+});
+workProcess.on('error', function (err) {
+    console.log('error:', err);
+});
+workProcess.on('close', function (code) {
+    console.log('close:', code);
+});
 
 child_process.exec('dir', {'timeout': 10 * 1000}, function (err, stdout, stderr) {
     console.log('exec: ======================================');
